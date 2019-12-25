ea1a5418404975909b062393d5278014ee7b7931
Матвей <matvey.starshiy@mail.ru>
2019-12-25 06:49:57 +0300

    labs

---
+++ Lab20/Lab20.5/Lab20.5.cpp
@@ -0,0 +1,28 @@
+﻿// Lab20.5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string> 
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int kol = 0,len,count=0,pos=0;
+	string str, str1;
+	cout << "Введите строку 1:\n";
+	getline(cin, str);
+	cout << "Введите строку 2:\n";
+	getline(cin, str1);
+	len = str1.length();
+	while ((pos = str.find(str1, pos)) != string::npos)
+	{
+		pos += len;
+		++count;
+	}
+
+		cout << "Found " << count << " matches" << std::endl;
+
+	system("pause");
+	return 0;
+}
---
+++ Lab22/Lab22.3/Lab22.3.cpp
@@ -0,0 +1,46 @@
+﻿// Lab22.3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include <fstream>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	string s,s1, name,secondname;
+	fstream FirstFile,SecondFile;
+	cout << "Введите имя файла: " << endl;
+	getline(cin, name);
+	FirstFile.open(name);
+	if (FirstFile)
+	{
+		getline(FirstFile, s1, '\0');
+		FirstFile.close();
+	}
+	else cout << "Первый файл не существует" << endl;
+
+	cout << "Введите имя второго файла: " << endl;
+	getline(cin, secondname);
+	SecondFile.open(secondname);
+	if (SecondFile)
+	{
+		getline(SecondFile, s, '\0');
+		SecondFile.close();
+	}
+	else cout << "Второй файл не существует" << endl;
+
+	FirstFile.open(name, ios::out);
+	if (FirstFile)
+	{
+		FirstFile << s << s1;
+		cout << "Дело сделано!" << endl;
+		FirstFile.close();
+	}
+	else cout << "Файл не существует" << endl;
+}
+
+
+
